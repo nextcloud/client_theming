c235a891c99226184ce836bc2707fa6c4e077629
Roeland Jago Douma <roeland@famdouma.nl>
2016-08-22 19:59:19 +0200

    Fix copyright statement

--- nextcloudtheme/nextcloudtheme.h
+++ nextcloudtheme/nextcloudtheme.h
@@ -77,7 +77,9 @@ public:
                 .arg(MIRALL_VERSION_STRING).arg("http://" MIRALL_STRINGIFY(APPLICATION_DOMAIN))
                 .arg(MIRALL_STRINGIFY(APPLICATION_DOMAIN));
 
-        re += tr("<p>Copyright ownCloud, Incorporated</p>");
+        re += trUtf8("<p><small>By Klaas Freitag, Daniel Molkentin, Jan-Christoph Borchardt, "
+                     "Olivier Goffart, Markus Götz and others.</small></p>");
+
         re += tr("<p>Distributed by %1 and licensed under the GNU General Public License (GPL) Version 2.0.<br/>"
              "%2 and the %2 Logo are registered trademarks of %1 in the "
              "European Union, other countries, or both.</p>")
