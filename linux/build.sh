f9f5cea4a8842815d4efc6468c8fe82c9ecf259e
probonopd <probonopd@users.noreply.github.com>
2016-10-01 17:09:02 +0200

    Create build.sh

---
+++ linux/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+sudo sh -c "echo 'deb-src http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_14.04/Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+sudo apt-get -y build-dep owncloud-client
+
+git submodule update --init --recursive
+mkdir build-linux
+cd build-linux
+cmake -D CMAKE_INSTALL_PREFIX=/app -D OEM_THEME_DIR=`pwd`/../nextcloudtheme ../client
+make
