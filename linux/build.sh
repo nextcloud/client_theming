e6223521c3140aad80ed2f69d57f22d32a626e2a
probonopd <probonopd@users.noreply.github.com>
2016-10-01 17:37:46 +0200

    Update build.sh

--- linux/build.sh
+++ linux/build.sh
@@ -17,4 +17,104 @@ make
 find . 
 sudo make install
 find /app
-exit 0
+
+########################################################################
+# Package the binaries built on Travis-CI as an AppImage
+# By Simon Peter 2016
+# For more information, see http://appimage.org/
+########################################################################
+
+export ARCH=$(arch)
+
+APP=NextCloud
+LOWERAPP=${APP,,}
+
+GIT_REV=$(git rev-parse --short HEAD)
+echo $GIT_REV
+
+mkdir -p $HOME/$APP/$APP.AppDir/usr/bin/
+
+cd $HOME/$APP/
+
+wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh -O ./functions.sh
+. ./functions.sh
+
+cd $APP.AppDir
+
+sudo chown -R $USER /app/
+sed -i -e 's|/app|././|g' /app/bin/nextcloud
+
+cp /app/bin/nextcloud ./usr/bin/
+
+########################################################################
+# Copy desktop and icon file to AppDir for AppRun to pick them up
+########################################################################
+
+get_apprun
+
+cp /app/share/applications/nextcloud.desktop .
+cp /app/share/icons/hicolor/256x256/apps/Nextcloud_ok.png nextcloud.png
+
+########################################################################
+# Copy in the dependencies that cannot be assumed to be available
+# on all target systems
+########################################################################
+
+# FIXME: How to find out which subset of plugins is really needed?
+mkdir -p ./usr/lib/qt4/plugins/
+PLUGINS=/usr/lib/x86_64-linux-gnu/qt4/plugins/
+cp -r $PLUGINS/* ./usr/lib/qt4/plugins/
+
+copy_deps
+
+########################################################################
+# Delete stuff that should not go into the AppImage
+########################################################################
+
+# Delete dangerous libraries; see
+# https://github.com/probonopd/AppImages/blob/master/excludelist
+delete_blacklisted
+
+# We don't bundle the developer stuff
+rm -rf usr/include || true
+rm -rf usr/lib/cmake || true
+rm -rf usr/lib/pkgconfig || true
+find . -name '*.la' | xargs -i rm {}
+strip usr/bin/* usr/lib/* || true
+
+########################################################################
+# desktopintegration asks the user on first run to install a menu item
+########################################################################
+
+get_desktopintegration $LOWERAPP
+
+########################################################################
+# Determine the version of the app; also include needed glibc version
+########################################################################
+
+GLIBC_NEEDED=$(glibc_needed)
+VERSION=git$GIT_REV-glibc$GLIBC_NEEDED
+
+########################################################################
+# Patch away absolute paths; it would be nice if they were relative
+########################################################################
+
+# patch_usr
+# Possibly need to patch additional hardcoded paths away, replace
+# "/usr" with "././" which means "usr/ in the AppDir"
+
+########################################################################
+# AppDir complete
+# Now packaging it as an AppImage
+########################################################################
+
+cd .. # Go out of AppImage
+
+mkdir -p ../out/
+generate_appimage
+
+########################################################################
+# Upload the AppDir
+########################################################################
+
+transfer ../out/*
