a480beb7ab3bb063d536e264d7b69e29d2a93c4c
probonopd <probonopd@users.noreply.github.com>
2016-10-01 18:10:15 +0200

    Update build.sh

--- linux/build.sh
+++ linux/build.sh
@@ -112,7 +112,7 @@ VERSION=git$GIT_REV-glibc$GLIBC_NEEDED
 cd .. # Go out of AppImage
 
 mkdir -p ../out/
-generate_appimage
+generate_type2_appimage
 
 ########################################################################
 # Upload the AppDir
