474292c8abd43294e39697d6c23babe7604a7bf8
István Váradi <ivaradi@varadiistvan.hu>
2017-01-20 20:31:53 +0100

    Paralel build support

--- linux/debian/scripts/config.sh
+++ linux/debian/scripts/config.sh
@@ -30,3 +30,6 @@ NEXTCLOUD_CLIENT_FULL_VERSION="${NEXTCLOUD_CLIENT_VERSION}-1.2~@DISTRIBUTION@1"
 
 # The tag of the nextcloud-client package
 NEXTCLOUD_CLIENT_TAG="v${NEXTCLOUD_CLIENT_VERSION}"
+
+# The number of CPUs that can be used for paralel builds
+NUMCPUS=4
--- linux/debian/scripts/pbuilder_all.sh
+++ linux/debian/scripts/pbuilder_all.sh
@@ -18,8 +18,8 @@ rm -f "${resultdir}/"*
 source "${HOME}/.pbuilderrc"
 
 dscversion=`echo ${QTKEYCHAIN_FULL_VERSION} | sed "s:@DISTRIBUTION@:${distribution}:g"`
-pbuilder-dist "${distribution}" build --othermirror "${OTHERMIRROR}" "$@" "${BUILDAREA}/qtkeychain_${dscversion}.dsc"
+pbuilder-dist "${distribution}" build --othermirror "${OTHERMIRROR}" --debbuildopts "-j${NUMCPUS}" "$@" "${BUILDAREA}/qtkeychain_${dscversion}.dsc"
 cp "${resultdir}/"*.deb "${PBUILDER_DEPS}"
 
 dscversion=`echo ${NEXTCLOUD_CLIENT_FULL_VERSION} | sed "s:@DISTRIBUTION@:${distribution}:g"`
-pbuilder-dist "${distribution}" build --othermirror "${OTHERMIRROR}" "$@" "${BUILDAREA}/nextcloud-client_${dscversion}.dsc"
+pbuilder-dist "${distribution}" build --othermirror "${OTHERMIRROR}" --debbuildopts "-j${NUMCPUS}" "$@" "${BUILDAREA}/nextcloud-client_${dscversion}.dsc"
