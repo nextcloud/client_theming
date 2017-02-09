fdd35838fa0b982463c16bc65d8e89ee9a739947
probonopd <probonopd@users.noreply.github.com>
2017-02-09 21:01:26 +0100

    Qt 5.8

--- linux/build.sh
+++ linux/build.sh
@@ -4,12 +4,16 @@
 # Build as per the instructions, but install in /app rather than /usr
 ########################################################################
 
+sudo add-apt-repository ppa:beineri/opt-qt58-trusty
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 sudo sh -c "echo 'deb-src http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_14.04/Release.key
 sudo apt-key add - < Release.key
 sudo apt-get update
 sudo apt-get -y build-dep owncloud-client
+sudo apt-get install -y qt58base
+
+. /opt/qt58/bin/qt58-env.sh
 
 git submodule update --init --recursive
 mkdir build-linux
