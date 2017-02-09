03a9c3d05d4b3772f9a309362e04a62e59f57876
probonopd <probonopd@users.noreply.github.com>
2017-02-09 22:09:53 +0100

    Only bundle selected plugins

--- linux/build.sh
+++ linux/build.sh
@@ -65,7 +65,7 @@ cp /app/share/icons/hicolor/256x256/apps/Nextcloud.png nextcloud.png
 # FIXME: How to find out which subset of plugins is really needed?
 mkdir -p ./usr/lib/qt4/plugins/
 PLUGINS=/usr/lib/x86_64-linux-gnu/qt4/plugins/
-cp -r $PLUGINS/* ./usr/lib/qt4/plugins/
+cp -r $PLUGINS/{bearer,generic,imageformats,platforminputcontexts,platforms,platformthemes} ./usr/lib/qt4/plugins/
 
 copy_deps
 
