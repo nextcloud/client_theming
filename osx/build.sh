b6b2205f1c9f632e7f24bede07eed24eecb6b9c2
Lukas Reschke <lukas@statuscode.ch>
2017-02-16 20:02:01 +0100

    Disable Shibboleth
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- osx/build.sh
+++ osx/build.sh
@@ -26,7 +26,7 @@ cp client_theming/osx/dsa_pub.pem client/admin/osx/sparkle/
 rm -rf build-mac
 mkdir build-mac
 cd build-mac
-cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=/Users/builder/install -DCMAKE_PREFIX_PATH=/Users/builder/Qt/5.6/clang_64 -D SPARKLE_INCLUDE_DIR=/Users/builder/Library/Frameworks/Sparkle.framework/ -D SPARKLE_LIBRARY=/Users/builder/Library/Frameworks/Sparkle.framework/ -D OEM_THEME_DIR=/Users/builder/client_theming/nextcloudtheme -DWITH_CRASHREPORTER=ON -DMIRALL_VERSION_BUILD=1 ../client
+cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=/Users/builder/install -DCMAKE_PREFIX_PATH=/Users/builder/Qt/5.6/clang_64 -D SPARKLE_INCLUDE_DIR=/Users/builder/Library/Frameworks/Sparkle.framework/ -D SPARKLE_LIBRARY=/Users/builder/Library/Frameworks/Sparkle.framework/ -D OEM_THEME_DIR=/Users/builder/client_theming/nextcloudtheme -DWITH_CRASHREPORTER=ON -DNO_SHIBBOLETH=1 -DMIRALL_VERSION_BUILD=1 ../client
 make
 sudo make install
 sudo ~/client_theming/client/admin/osx/sign_app.sh ~/install/nextcloud.app 14C6139C495F80E3086E9912410161BFB41F0578
