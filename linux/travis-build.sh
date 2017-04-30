b6c0f095259eeb30fcc346ac45d9e340de228987
István Váradi <ivaradi@varadiistvan.hu>
2017-04-30 17:30:00 +0200

    Added signing key #4

--- linux/travis-build.sh
+++ linux/travis-build.sh
@@ -103,7 +103,7 @@ elif [ "$BUILD_TYPE" == "debian" ]; then
         cp /tmp/tmpchangelog debian/changelog
         cat linux/debian/nextcloud-client/debian/changelog >> debian/changelog
 
-        /usr/bin/debuild -S -k 7D14AA7B
+        /usr/bin/env DEBSIGN_KEYID=7D14AA7B /usr/bin/debuild -S
     fi
 else
     echo 'No $BUILD_TYPE defined'
