47cc1c0f97607dcf09928af6b12b82198898a4b3
István Váradi <ivaradi@varadiistvan.hu>
2017-04-30 17:23:50 +0200

    Added signing key #3

--- linux/travis-build.sh
+++ linux/travis-build.sh
@@ -103,7 +103,7 @@ elif [ "$BUILD_TYPE" == "debian" ]; then
         cp /tmp/tmpchangelog debian/changelog
         cat linux/debian/nextcloud-client/debian/changelog >> debian/changelog
 
-        /usr/bin/debuild -S --username "Nextcloud Developers (This key is to be used in automation scripts by Nextcloud) <ivaradi@varadiistvan.hu>"
+        /usr/bin/debuild -S -k 7D14AA7B
     fi
 else
     echo 'No $BUILD_TYPE defined'
