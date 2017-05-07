392e3e3e1cfa0d54c1cecc15a6b168d90bfce911
István Váradi <ivaradi@varadiistvan.hu>
2017-05-07 19:23:38 +0200

    .oscrc is decrypted from the correct file

--- linux/debian/travis-build.sh
+++ linux/debian/travis-build.sh
@@ -16,7 +16,7 @@ if [ "$TRAVIS_BUILD_STEP" == "install" ]; then
         openssl aes-256-cbc -K $encrypted_585e03da75ed_key -iv $encrypted_585e03da75ed_iv -in linux/debian/signing-key.txt.enc -d | gpg --import
         echo "DEBUILD_DPKG_BUILDPACKAGE_OPTS='-k7D14AA7B'" >> ~/.devscripts
     fi
-    openssl aes-256-cbc -K $encrypted_8da7a4416c7a_key -iv $encrypted_8da7a4416c7a_iv -in oscrc.enc -out ~/.oscrc -d
+    openssl aes-256-cbc -K $encrypted_8da7a4416c7a_key -iv $encrypted_8da7a4416c7a_iv -in linux/debian/oscrc.enc -out ~/.oscrc -d
 
 elif [ "$TRAVIS_BUILD_STEP" == "script" ]; then
     #pwd
