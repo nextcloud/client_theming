a2ecc8de4e3eea56a1374e625ff3dd60551d93e5
Taylor Smith <tsmith@tsmithcreative.com>
2017-10-04 05:41:45 +0000

    Make a Travis build script for Windows, modeled after the Debian script

---
+++ win/travis-build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Adapted from the Debian travis-build.sh to test building a Windows binary
+# according to the build instructions in README.md
+
+set -xe
+shopt -s extglob
+
+TRAVIS_BUILD_STEP="$1"
+
+PPA=ppa:nextcloud-devs/client
+PPA_BETA=ppa:nextcloud-devs/client-beta
+
+OBS_PROJECT=home:ivaradi
+OBS_PROJECT_BETA=home:ivaradi:beta
+OBS_PACKAGE=nextcloud-client
+
+if [ "$TRAVIS_BUILD_STEP" == "install" ]; then
+    sudo apt-get update -q
+    sudo apt-get install -y devscripts cdbs osc
+
+    if test "$encrypted_585e03da75ed_key" -a "$encrypted_585e03da75ed_iv"; then
+        openssl aes-256-cbc -K $encrypted_585e03da75ed_key -iv $encrypted_585e03da75ed_iv -in linux/debian/signing-key.txt.enc -d | gpg --import
+        echo "DEBUILD_DPKG_BUILDPACKAGE_OPTS='-k7D14AA7B'" >> ~/.devscripts
+
+        openssl aes-256-cbc -K $encrypted_585e03da75ed_key -iv $encrypted_585e03da75ed_iv -in linux/debian/oscrc.enc -out ~/.oscrc -d
+    elif test "$encrypted_8da7a4416c7a_key" -a "$encrypted_8da7a4416c7a_iv"; then
+        openssl aes-256-cbc -K $encrypted_8da7a4416c7a_key -iv $encrypted_8da7a4416c7a_iv -in linux/debian/oscrc.enc -out ~/.oscrc -d
+        PPA=ppa:ivaradi/nextcloud-client-exp
+    fi
+
+elif [ "$TRAVIS_BUILD_STEP" == "script" ]; then
+    read basever kind <<<$(linux/debian/scripts/git2changelog.py /tmp/tmpchangelog stable)
+
+    docker build -t nextcloud-client-win32:${basever} client/admin/win/docker/
+    docker run -v "$PWD:/home/user/" nextcloud-client-win32:${basever} /home/user/win/build.sh $(id -u)
+
+    cd ..
+
+    echo "$kind" > kind
+
+    if test "$kind" = "beta"; then
+        repo=client-beta
+    else
+        repo=client
+    fi
+
+    origsourceopt=""
+    #if ! wget http://ppa.launchpad.net/ivaradi/nextcloud-client-exp/ubuntu/pool/main/n/nextcloud-client/nextcloud-client_${basever}.orig.tar.bz2; then
+    if ! wget http://ppa.launchpad.net/nextcloud-devs/${repo}/ubuntu/pool/main/n/nextcloud-client/nextcloud-client_${basever}.orig.tar.bz2; then
+        mv client_theming nextcloud-client_${basever}
+        tar cjf nextcloud-client_${basever}.orig.tar.bz2 --exclude .git nextcloud-client_${basever}
+        mv nextcloud-client_${basever} client_theming
+        origsourceopt="-sa"
+    fi
+fi
